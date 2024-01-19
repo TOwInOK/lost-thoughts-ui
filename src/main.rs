2e8fa85759d0649f733b302c25a9034fef440921
TOwInOK <60252419+TOwInOK@users.noreply.github.com>
2024-01-19 12:44:28 +0700

    Now we can use get reqwest fix margin into debug buttons fix stupid title naming

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::io::ErrorKind;
-use serde::{Deserialize, Serialize};
-use iced::widget::{button, column, horizontal_space, row, text_input, vertical_space, Column};
+use iced::widget::{button, column, horizontal_space, row, text_input, vertical_space};
 use iced::{executor, Application, Font, Length, Theme};
 use iced::{Command, Settings};
+use reqwest::{Client, StatusCode};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 fn main() -> iced::Result {
@@ -13,7 +13,7 @@ fn main() -> iced::Result {
     })
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct LostThoughts {
     user: User,
     current_window: WindowState,
@@ -24,7 +24,7 @@ struct LostThoughts {
     posts: Vec<Post>,
 }
 
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize, Debug)]
 struct User {
     login: String,
     password: String,
@@ -33,24 +33,23 @@ struct User {
     role: Role,
 }
 
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize, Debug)]
 struct user_min {
     login: String,
     #[serde(default = "default_role")]
     role: Role,
 }
-    
+
 fn default_role() -> Role {
     Role::Default
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum Role {
     Admin,
     Paid,
     Default,
 }
-
 
 #[derive(Debug, Clone)]
 struct Post {}
@@ -68,8 +67,9 @@ enum WindowState {
 #[derive(Debug, Clone)]
 enum Message {
     SignIn,
+    Signed(Result<StatusCode, String>),
     SignUp,
-    SwitchWindow(WindowState, String),
+    SwitchWindow(WindowState),
     PasswordChange(String),
     LoginChange(String),
     EmailChange(String),
@@ -113,37 +113,84 @@ impl Application for LostThoughts {
 
     fn update(&mut self, message: Self::Message) -> iced::Command<Self::Message> {
         match message {
-            Message::SignIn => (),
-            Message::SignUp => (),
-            Message::SwitchWindow(window, name_of_window) => {
-                self.current_window = window;
-                self.title = name_of_window;
+            Message::SignIn => {
+                Command::perform(log_in(self.user.clone()), |result| Message::Signed(result))
+            }
+            Message::SignUp => {
+                Command::perform(log_in(self.user.clone()), |result| Message::Signed(result))
+            }
+            Message::SwitchWindow(window) => {
+                match window {
+                    WindowState::Login => {
+                        self.current_window = window;
+                        self.title = "Login".to_owned();
+                    }
+                    WindowState::Register => {
+                        self.current_window = window;
+                        self.title = "Register".to_owned();
+                    }
+                    WindowState::Main => {
+                        self.current_window = window;
+                        self.title = "Main".to_owned();
+                    }
+                    WindowState::Account => {
+                        self.current_window = window;
+                        self.title = "Account".to_owned();
+                    }
+                    WindowState::Poster => {
+                        self.current_window = window;
+                        self.title = "Poster".to_owned();
+                    }
+                    WindowState::Search => {
+                        self.current_window = window;
+                        self.title = "Search".to_owned();
+                    }
+                }
+
+                Command::none()
             }
             Message::PasswordChange(password) => {
                 self.user.password = password;
+                Command::none()
             }
             Message::LoginChange(login) => {
                 self.user.login = login;
+                Command::none()
             }
             Message::EmailChange(email) => {
                 self.user.email = email;
+                Command::none()
             }
-            Message::SearchChange(value, ) => {
+            Message::SearchChange(value) => {
                 self.search = value;
+                Command::none()
             }
             Message::PostAdd(_) => todo!(),
             Message::DebugSwitch(value) => {
+                println!("debbug is {}", value);
                 self.debbug = value;
+                Command::none()
             }
+            Message::Signed(result) => match result {
+                Ok(e) => match e {
+                    StatusCode::OK => {
+                        println!("{}", e);
+                        Command::perform(async {}, |_| Message::SwitchWindow(WindowState::Main))
+                    }
+                    _ => {
+                        println!("{}", e);
+                        Command::none()
+                    }
+                },
+                Err(e) => Command::none(),
+            },
         }
-        Command::none()
     }
 
     fn view(&self) -> iced::Element<'_, Self::Message, iced::Renderer<Self::Theme>> {
         let content = match self.current_window {
             WindowState::Login => column![
-                text_input("Login", &self.user.login)
-                    .on_input(Message::LoginChange),
+                text_input("Login", &self.user.login).on_input(Message::LoginChange),
                 text_input("Password", &self.user.password)
                     .password()
                     .on_input(Message::PasswordChange),
@@ -151,7 +198,7 @@ impl Application for LostThoughts {
                     horizontal_space(Length::Fill),
                     button("Submit").on_press(Message::SignIn),
                     button("Has no account?")
-                        .on_press(Message::SwitchWindow(WindowState::Register, "Register".to_string())),
+                        .on_press(Message::SwitchWindow(WindowState::Register)),
                     horizontal_space(Length::Fill),
                 ]
                 .spacing(10)
@@ -160,19 +207,17 @@ impl Application for LostThoughts {
             .padding(30)
             .spacing(20),
             WindowState::Register => column![
-                text_input("Login", &self.user.login)
-                    .on_input(Message::LoginChange),
+                text_input("Login", &self.user.login).on_input(Message::LoginChange),
                 text_input("Password", &self.user.password)
                     .password()
                     .on_input(Message::PasswordChange),
                 text_input("Confirm Password", &self.user.password),
-                text_input("Email", &self.user.email)
-                    .on_input(Message::EmailChange),
+                text_input("Email", &self.user.email).on_input(Message::EmailChange),
                 row![
                     horizontal_space(Length::Fill),
                     button("Submit").on_press(Message::SignUp),
                     button("Already have an account?")
-                        .on_press(Message::SwitchWindow(WindowState::Login, "Login".to_string())),
+                        .on_press(Message::SwitchWindow(WindowState::Login)),
                     horizontal_space(Length::Fill),
                 ]
                 .spacing(10)
@@ -189,12 +234,12 @@ impl Application for LostThoughts {
         let debbug_menu = if self.debbug {
             row![
                 horizontal_space(Length::Fill),
-                button("login").on_press(Message::SwitchWindow(WindowState::Login, "Login".to_string())),
-                button("register").on_press(Message::SwitchWindow(WindowState::Register, "Register".to_string())),
-                button("main").on_press(Message::SwitchWindow(WindowState::Main, "Main".to_string())),
-                button("account").on_press(Message::SwitchWindow(WindowState::Account, "Account".to_string())),
-                button("poster").on_press(Message::SwitchWindow(WindowState::Poster, "Poster".to_string())),
-                button("search").on_press(Message::SwitchWindow(WindowState::Search, "Search".to_string())),
+                button("login").on_press(Message::SwitchWindow(WindowState::Login)),
+                button("register").on_press(Message::SwitchWindow(WindowState::Register)),
+                button("main").on_press(Message::SwitchWindow(WindowState::Main)),
+                button("account").on_press(Message::SwitchWindow(WindowState::Account)),
+                button("poster").on_press(Message::SwitchWindow(WindowState::Poster)),
+                button("search").on_press(Message::SwitchWindow(WindowState::Search)),
                 horizontal_space(Length::Fill),
                 button("debbug off").on_press(Message::DebugSwitch(false)),
             ]
@@ -205,7 +250,6 @@ impl Application for LostThoughts {
                 horizontal_space(Length::Fill),
                 button("debbug on").on_press(Message::DebugSwitch(true)),
             ]
-            .padding(30)
         };
 
         column![content, vertical_space(Length::Fill), debbug_menu]
@@ -217,8 +261,28 @@ impl Application for LostThoughts {
         Theme::Dark
     }
 }
-
-
-async fn log_in(user: User) -> Result<(), ErrorKind> {
-    todo!()
+async fn log_in(user: User) -> Result<StatusCode, String> {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            let client = Client::new();
+            client
+                .get(format!(
+                    "https://api.lost-umbrella.com/user/{}/settings",
+                    &user.login
+                ))
+                .json(&json!({
+                    "name": &user.login,
+                    "password": &user.password,
+                }))
+                .send()
+                .await
+                .map(|e| {
+                    println!("{:#?}", &e.status());
+                    e.status()
+                })
+                .map_err(|e| e.to_string())
+        })
 }
