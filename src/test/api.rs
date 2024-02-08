a6149d83de049305a50df286430c0619a4f5a958
TOwInOK <60252419+TOwInOK@users.noreply.github.com>
2024-02-08 12:35:30 +0700

    Add test for delete and push fns

--- src/test/api.rs
+++ src/test/api.rs
@@ -267,3 +267,145 @@ pub async fn get_post_by_id() {
     //As we use json like User we neet to use .is_ok()
     assert!(result.is_ok());
 }
+
+#[tokio::test]
+pub async fn push() {
+    // Запуск фиктивного сервера
+    let mut lost_thought = mockito::Server::new();
+    lost_thought
+        .mock("PUT", "/endpoint")
+        //match reqwest with mock data
+        .match_body(
+            r#"
+            {
+               	"post":{
+              		"_id": {
+             			"$oid": "65c4656f81062592f2274a66"
+              		},
+              		"author": [
+             			"example_user"
+              		],
+              		"date": 1703179315411,
+              		"underlabel": "test",
+              		"label": "TEST",
+              		"text": "TEF",
+              		"footer": "FOOTER?",
+              		"tags": [
+             			"1",
+             			"2"
+              		]
+               	},
+              		"auth": {
+              		"name": "example_user",
+              		"password": "test123"
+               	}
+            }
+            "#,
+        )
+        .with_body(r#"Post edited"#)
+        .create();
+    let uri = format!("{}/endpoint", lost_thought.url());
+
+    let client = Client::new();
+    let response = client
+        .put(format!("{}", uri))
+        .body(
+            r#"
+            {
+               	"post":{
+              		"_id": {
+             			"$oid": "65c4656f81062592f2274a66"
+              		},
+              		"author": [
+             			"example_user"
+              		],
+              		"date": 1703179315411,
+              		"underlabel": "test",
+              		"label": "TEST",
+              		"text": "TEF",
+              		"footer": "FOOTER?",
+              		"tags": [
+             			"1",
+             			"2"
+              		]
+               	},
+              		"auth": {
+              		"name": "example_user",
+              		"password": "test123"
+               	}
+            }
+            "#,
+        )
+        .send()
+        .await
+        .map_err(|e| IOErrors::SingIn(e.to_string()))
+        .unwrap();
+    println!("Send & Get");
+    let result: Result<Option<String>, IOErrors> = if response.status().is_success() {
+        println!("Get Json");
+        let text = response
+            .text()
+            .await
+            .map_err(|e| IOErrors::SingIn(e.to_string()))
+            .unwrap();
+
+        Ok(Some(text))
+    } else {
+        println!("Get Nothing: {}", response.status());
+        Err(IOErrors::SingUp("".to_owned()))
+    };
+    //As we use json like User we neet to use .is_ok()
+    assert!(result.is_ok());
+}
+
+#[tokio::test]
+pub async fn delete_post() {
+    // Запуск фиктивного сервера
+    let mut lost_thought = mockito::Server::new();
+    lost_thought
+        .mock("PUT", "/endpoint")
+        //match reqwest with mock data
+        .match_body(
+            r#"
+            {
+          		"name": "example_user",
+          		"password": "test123"
+            }
+            "#,
+        )
+        .with_body(r#"Post edited"#)
+        .create();
+    let uri = format!("{}/endpoint", lost_thought.url());
+
+    let client = Client::new();
+    let response = client
+        .put(format!("{}", uri))
+        .body(
+            r#"
+            {
+          		"name": "example_user",
+          		"password": "test123"
+            }
+            "#,
+        )
+        .send()
+        .await
+        .map_err(|e| IOErrors::SingIn(e.to_string()))
+        .unwrap();
+    println!("Send & Get");
+    let result: Result<Option<String>, IOErrors> = if response.status().is_success() {
+        println!("Get Json");
+        let text = response
+            .text()
+            .await
+            .map_err(|e| IOErrors::SingIn(e.to_string()))
+            .unwrap();
+
+        Ok(Some(text))
+    } else {
+        println!("Get Nothing: {}", response.status());
+        Err(IOErrors::SingUp("".to_owned()))
+    };
+    //As we use json like User we neet to use .is_ok()
+    assert!(result.is_ok());
+}
